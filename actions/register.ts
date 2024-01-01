eebb11c30ca5387d91a831d4b8db3845aa8d43a3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-01 17:20:13 +0000

    user data fetcher implemented

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,7 @@ import * as z from "zod";
 
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -18,9 +19,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({
-    where: { email },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser)
     return {
