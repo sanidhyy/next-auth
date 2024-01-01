629270c71a6819cda6c3eff4a088283b83d996af
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-01 16:43:34 +0000

    register schema created

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
     message: "Password is required.",
   }),
 });
+
+export const RegisterSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required.",
+  }),
+  email: z.string().email({
+    message: "Email is required.",
+  }),
+  password: z.string().trim().min(6, {
+    message: "Minimum 6 characters required.",
+  }),
+});
