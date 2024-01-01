8cfb8854590b92fb8af975b2566c05900deb430c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-01 14:49:58 +0000

    css reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
-      <p className="text-4xl font-extrabold text-green-500">Hello, World!</p>
+      <p className="text-4xl font-extrabold text-red-500">Hello, World!</p>
+      <Button variant="destructive" size="lg">
+        Click Me
+      </Button>
     </main>
   );
 }
