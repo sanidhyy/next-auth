37bf34c6fc633bb7e44e81f36413ecf772280947
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 06:09:39 +0000

    [temp] sign out button added

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,24 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 const SettingsPage = async () => {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+
+      <form
+        action={async () => {
+          "use server";
+
+          await signOut();
+        }}
+      >
+        <Button type="submit">Sign out</Button>
+      </form>
+    </div>
+  );
 };
 
 export default SettingsPage;
