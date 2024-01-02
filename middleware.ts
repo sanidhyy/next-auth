45cbad43b3b6035326ad1c643c866da205783514
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 05:54:59 +0000

    middleware redirects implemented

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,36 @@
 import NextAuth from "next-auth";
 
 import authConfig from "@/auth.config";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
-  console.log(req.nextUrl.pathname);
-  console.log(isLoggedIn);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) return null;
+
+  if (isAuthRoute) {
+    if (isLoggedIn)
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute)
+    return Response.redirect(new URL("/auth/login", nextUrl));
+
+  return null;
 });
 
 export const config = {
