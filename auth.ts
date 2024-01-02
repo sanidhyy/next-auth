d543d1ea68d25f2a856f9502ce97138d6afcef87
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 06:44:25 +0000

    check email verification before login

--- auth.ts
+++ auth.ts
@@ -13,6 +13,13 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
+    async signIn({ user }) {
+      const existingUser = await getUserById(user.id);
+
+      if (!existingUser || !existingUser.emailVerified) return false;
+
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) session.user.id = token.sub;
 
