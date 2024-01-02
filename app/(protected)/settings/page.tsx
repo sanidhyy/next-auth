06d49c350f0f91e0b1d9f513414d1d600bc4b1a0
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 17:14:20 +0000

    account settings page implemented

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,16 +1,102 @@
 "use client";
 
-import { logout } from "@/actions/logout";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useSession } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { settings } from "@/actions/settings";
 import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import { Input } from "@/components/ui/input";
 import { useCurrentUser } from "@/hooks/use-current-user";
+import { SettingsSchema } from "@/schemas";
 
 const SettingsPage = () => {
-  const session = useCurrentUser();
+  const user = useCurrentUser();
 
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const { update } = useSession();
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: user?.name || undefined,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
+    startTransition(() => {
+      settings(values)
+        .then((data) => {
+          if (data.error) setError(data.error);
+
+          if (data.success) {
+            update();
+            setSuccess(data.success);
+          }
+        })
+        .catch(() => setError("Something went wrong."));
+    });
+  };
   return (
-    <div className="bg-white p-10 rounded-xl">
-      <Button onClick={() => logout()}>Sign out</Button>
-    </div>
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">⚙ Settings</p>
+      </CardHeader>
+
+      <CardContent>
+        <Form {...form}>
+          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="John Doe"
+                        disabled={isPending}
+                        aria-disabled={isPending}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormError message={error} />
+            <FormSuccess message={success} />
+
+            <Button
+              type="submit"
+              disabled={isPending}
+              aria-disabled={isPending}
+            >
+              Save
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
 
