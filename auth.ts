8a126d786a23433cc0984ff438d6096b2475b11c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 06:40:36 +0000

    auth type fixes

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import type { UserRole } from "@prisma/client";
 import NextAuth from "next-auth";
 
+import { getUserById } from "@/data/user";
 import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
 
@@ -14,9 +16,19 @@ export const {
     async session({ token, session }) {
       if (token.sub && session.user) session.user.id = token.sub;
 
+      if (token.role && session.user)
+        session.user.role = token.role as UserRole;
+
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
       return token;
     },
   },
