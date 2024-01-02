2afc5d9956339a5f5bfab5344410c58212c4150d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 06:24:37 +0000

    user role field added

--- auth.ts
+++ auth.ts
@@ -10,6 +10,16 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) session.user.id = token.sub;
+
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
