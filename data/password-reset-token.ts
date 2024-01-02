f45291f70a618904a85d57bf5f1e7d6206e091cd
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 13:24:36 +0000

    password reset form data fetcher implemented

---
+++ data/password-reset-token.ts
@@ -0,0 +1,32 @@
+import { db } from "@/lib/db";
+
+export const getPasswordResetTokenByToken = async (token: string) => {
+  try {
+    const passwordResetToken = await db.passwordResetToken.findUnique({
+      where: {
+        token,
+      },
+    });
+
+    return passwordResetToken;
+  } catch {
+    return null;
+  }
+};
+
+export const getPasswordResetTokenByEmail = async (email: string) => {
+  try {
+    const passwordResetToken = await db.passwordResetToken.findFirst({
+      where: {
+        email,
+      },
+      orderBy: {
+        expires: "desc",
+      },
+    });
+
+    return passwordResetToken;
+  } catch {
+    return null;
+  }
+};
