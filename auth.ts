4fc1d75a1425642a0db0637816b021df056c79f5
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 15:40:15 +0000

    client page implemented

--- auth.ts
+++ auth.ts
@@ -63,6 +63,9 @@ export const {
       if (token.role && session.user)
         session.user.role = token.role as UserRole;
 
+      if (session.user)
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+
       return session;
     },
     async jwt({ token }) {
@@ -73,6 +76,7 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
       return token;
     },
   },
