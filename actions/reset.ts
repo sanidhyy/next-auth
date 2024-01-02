44657472b471c0947ece2a74e9815b7db0010216
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 13:25:05 +0000

    password reset mail functionality completed

--- actions/reset.ts
+++ actions/reset.ts
@@ -3,6 +3,8 @@
 import * as z from "zod";
 
 import { getUserByEmail } from "@/data/user";
+import { sendPasswordResetEmail } from "@/lib/mail";
+import { generatePasswordResetToken } from "@/lib/tokens";
 import { ResetSchema } from "@/schemas";
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
@@ -16,7 +18,11 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
 
   if (!existingUser) return { error: "Email not found." };
 
-  // TODO: Generate token and send email
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(
+    passwordResetToken.email,
+    passwordResetToken.token
+  );
 
   return { success: "Reset email sent." };
 };
