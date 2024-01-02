65640feb859c0d4e95c430e3ab2505fb65b633c8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 13:11:36 +0000

    reset password route added to public routes

--- routes.ts
+++ routes.ts
@@ -3,7 +3,11 @@
  * These routes do not require authentication.
  * @type {string[]}
  **/
-export const publicRoutes: string[] = ["/", "/auth/new-verification"];
+export const publicRoutes: string[] = [
+  "/",
+  "/auth/new-verification",
+  "/auth/reset",
+];
 
 /**
  * An array of routes that are used for authentication.
