ac5de04571e7b116485fee466d48bb9012000052
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-02 15:08:21 +0000

    changed expiration time to 5 min

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -9,7 +9,7 @@ import { db } from "./db";
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
-  const expires = new Date(new Date().getTime() + 3600 * 1000); // 1 hour
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000); // 5 minutes
 
   const existingToken = await getTwoFactorTokenByEmail(email);
 
