a7270ad5bae62a03a73e0fbbb30e78cec08f207a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 05:26:04 +0000

    logout button bug fix

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,17 +1,27 @@
 "use client";
 
 import type { PropsWithChildren } from "react";
+import { useTransition } from "react";
 
 import { logout } from "@/actions/logout";
 
 export const LogoutButton = ({ children }: PropsWithChildren) => {
+  const [isPending, startTransition] = useTransition();
+
   const onClick = () => {
-    logout();
+    startTransition(() => {
+      logout();
+    });
   };
 
   return (
-    <span onClick={onClick} className="cursor-pointer">
+    <button
+      disabled={isPending}
+      aria-disabled={isPending}
+      onClick={onClick}
+      className="w-full cursor-pointer"
+    >
       {children}
-    </span>
+    </button>
   );
 };
