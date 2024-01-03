d35bd153a0abec500e7e6a44ef320d9670904331
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 05:44:59 +0000

    site config added

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         >
           🔐 Auth
         </h1>
-        <p className="text-white text-lg">A simple authentication service</p>
+        <p className="text-white text-lg">A simple authentication service.</p>
 
         <div>
           <LoginButton mode="modal" asChild>
